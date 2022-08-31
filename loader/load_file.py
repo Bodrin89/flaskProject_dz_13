e570152d4b2df0da57f9f4f8c21b1ddcd367a89b
vladimirmac <vladimirbodrin89@gmail.com>
2022-08-31 21:26:38 +0300

    finish

--- loader/load_file.py
+++ loader/load_file.py
@@ -1,4 +1,4 @@
-
+import logging
 import uuid
 from flask import Blueprint, render_template, request
 from utils import check_extension, load_new_post, read_json_file
@@ -10,6 +10,8 @@ save_text_blueprint = Blueprint("save_text_blueprint", __name__)
 FILE = 'posts.json'
 data_list = read_json_file(FILE)
 ALL_EXTENSION = ['png', 'jpeg', 'jpg']
+
+
 @load_post_blueprint.route('/post')
 def load_page():
     load_file = render_template('post_form.html')
@@ -23,16 +25,21 @@ def save_foto():
     file_extension = foto.content_type.split('/')[-1]
     if foto:
         if check_extension(ALL_EXTENSION, file_extension):
-            path_img = f'uploads/img/{uuid.uuid4()}.{file_extension}'
+            path_img = f'static/img/{uuid.uuid4()}.{file_extension}'
             foto.save(path_img)
             new_json_data = {
                 "pic": path_img,
                 "content": f"{text}"
             }
             load_new_post(new_json_data, data_list, FILE)
+            ff = [new_json_data]
+            index_page = render_template('post_list.html', all_search_posts=ff)
+            return f"{index_page}"
         else:
+            logging.info('файл не фото')
             return 'файл не фото'
     else:
         return 'Файла нет'
-    return f"загружен файл {foto.filename} {text}|||| {file_extension}"
+
+
 
--- utils.py
+++ utils.py
@@ -1,15 +1,33 @@
-
 import json
+import logging
+from json import JSONDecodeError
 
 
-#Открыть JSON файл
+# Открыть JSON файл
 def read_json_file(data_json):
-    with open(data_json, 'r', encoding='utf-8') as file:
-        data = json.load(file)
+    try:
+        with open(data_json, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+    except FileNotFoundError as e:
+        logging.error(e)
+        print("Файл не найден")
+    except JSONDecodeError as e:
+        logging.error(e)
+        print('Не удалось получить JSON из файла')
+    else:
+        logging.info('Данные получены')
+        print('Данные получены')
         return data
 
 
-#Функция поиска постов по слову
+
+
+
+
+
+
+
+# Функция поиска постов по слову
 def load_posts(search_word, all_posts):
     posts = []
     for i in all_posts:
@@ -18,17 +36,15 @@ def load_posts(search_word, all_posts):
     return posts
 
 
-#Функция добавления нового поста в JSON файл
+# Функция добавления нового поста в JSON файл
 def load_new_post(load_data_json, data_js, file_json):
     data_js.append(load_data_json)
     with open(file_json, 'w', encoding='utf-8') as f:
         json.dump(data_js, f, ensure_ascii=False, indent=4)
 
 
-#Функция проверки расщирения файла
+# Функция проверки расщирения файла
 def check_extension(array_extension, extension):
     if extension.lower() in array_extension:
         return True
     return False
-
-
